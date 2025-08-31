92fb5e52b94ef4dd39ee33adddc0ba6de22922bb
Simone Basso <bassosimone@gmail.com>
2025-08-31 17:47:45 +0200

    fix(io.hpp): tighten checks around overflow

--- include/minimk/io.hpp
+++ include/minimk/io.hpp
@@ -18,16 +18,21 @@
 /// M_T_send must return nwritten > 0 when its return code is zero.
 template <typename T, minimk_error_t (*M_T_send)(T, const void *, size_t, size_t *)>
 minimk_error_t minimk_io_writeall(T sock, const void *vbuf, size_t count) noexcept {
+    // Allow pointer arithmetic and ensure overflow is impossible
     const char *buf = static_cast<const char *>(vbuf);
+    MINIMK_ASSERT(reinterpret_cast<uintptr_t>(buf) <= UINTPTR_MAX - count);
 
     for (size_t total = 0; total < count;) {
+        // Prepare for this round of doing I/O
         size_t nwritten = 0;
         size_t amount = count - total;
 
+        // Send inside a section where clang buffer safety is disabled
 #pragma clang unsafe_buffer_usage begin
         minimk_error_t rv = M_T_send(sock, buf + total, amount, &nwritten);
 #pragma clang unsafe_buffer_usage end
 
+        // Handle the failure cases
         if (rv == MINIMK_EINTR) {
             continue;
         }
@@ -35,7 +40,8 @@ minimk_error_t minimk_io_writeall(T sock, const void *vbuf, size_t count) noexce
             return rv; // all-or-nothing semantics
         }
 
-        MINIMK_ASSERT(nwritten > 0); // enforces documented assumptions
+        // Enforce documented assumptions on writing and move on
+        MINIMK_ASSERT(nwritten > 0);
         MINIMK_ASSERT(nwritten <= amount);
         total += nwritten;
     }
@@ -53,23 +59,30 @@ minimk_error_t minimk_io_writeall(T sock, const void *vbuf, size_t count) noexce
 /// M_T_recv must return nread > 0 when its return code is zero.
 template <typename T, minimk_error_t (*M_T_recv)(T, void *, size_t, size_t *)>
 minimk_error_t minimk_io_readall(T sock, void *vbuf, size_t count) noexcept {
+    // Allow pointer arithmetic and ensure overflow is impossible
     char *buf = static_cast<char *>(vbuf);
+    MINIMK_ASSERT(reinterpret_cast<uintptr_t>(buf) <= UINTPTR_MAX - count);
 
     for (size_t total = 0; total < count;) {
+        // Prepare for this round of doing I/O
         size_t nread = 0;
         size_t amount = count - total;
 
+        // Receive inside a section where clang buffer safety is disabled
 #pragma clang unsafe_buffer_usage begin
         minimk_error_t rv = M_T_recv(sock, buf + total, amount, &nread);
 #pragma clang unsafe_buffer_usage end
 
+        // Handle the failure cases
         if (rv == MINIMK_EINTR) {
             continue;
         }
         if (rv != 0) {
             return rv; // all-or-nothing semantics
         }
-        MINIMK_ASSERT(nread > 0); // enforces documented assumptions
+
+        // Enforce documented assumptions on reading and move on
+        MINIMK_ASSERT(nread > 0);
         MINIMK_ASSERT(nread <= amount);
         total += nread;
     }
