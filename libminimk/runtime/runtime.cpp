949c665f3ce84af3fc005aa6f568a4dc453dcf64
Simone Basso <bassosimone@gmail.com>
2025-08-31 10:16:07 +0200

    refactor(runtime): reduce code surface using s0

--- libminimk/runtime/runtime.cpp
+++ libminimk/runtime/runtime.cpp
@@ -60,82 +60,103 @@ minimk_error_t minimk_runtime_go(void (*entry)(void *opaque), void *opaque) noex
     return 0;
 }
 
-void minimk_runtime_run(void) noexcept {
+/// Function that runs the given scheduler until we're out of coroutines to run.
+static inline void minimk_runtime_scheduler_run(scheduler *sched) noexcept {
     // Ensure we are not yet inside the coroutine world.
-    MINIMK_ASSERT(s0.current == nullptr);
+    MINIMK_ASSERT(sched->current == nullptr);
 
     // Continue until we're out of coroutines.
-    for (size_t fair = 0; minimk_runtime_scheduler_count_nonnull_coroutines(&s0) > 0;) {
+    for (size_t fair = 0; minimk_runtime_scheduler_count_nonnull_coroutines(sched) > 0;) {
         // Check whether there are coroutines that need cleanup.
-        minimk_runtime_scheduler_clean_exited_coroutines(&s0);
+        minimk_runtime_scheduler_clean_exited_coroutines(sched);
 
         // Wakeup coroutines whose deadline has expired.
-        minimk_runtime_scheduler_maybe_expire_deadlines(&s0);
+        minimk_runtime_scheduler_maybe_expire_deadlines(sched);
 
         // Fairly select the first runnable coroutine.
-        s0.current = minimk_runtime_scheduler_pick_runnable(&s0, &fair);
+        sched->current = minimk_runtime_scheduler_pick_runnable(sched, &fair);
 
         // If there are no runnable coroutines, wait for something to
         // happen but avoid sleeping if everyine is dead.
-        if (s0.current == nullptr) {
-            if (minimk_runtime_scheduler_count_nonnull_coroutines(&s0) > 0) {
-                minimk_runtime_scheduler_block_on_poll(&s0);
+        if (sched->current == nullptr) {
+            if (minimk_runtime_scheduler_count_nonnull_coroutines(sched) > 0) {
+                minimk_runtime_scheduler_block_on_poll(sched);
             }
             continue;
         }
 
         // Transfer the control to the current coroutine
-        minimk_runtime_scheduler_switch(&s0);
+        minimk_runtime_scheduler_switch(sched);
 
         // We're now inside the scheduler again.
-        s0.current = nullptr;
+        sched->current = nullptr;
     }
 }
 
-void minimk_runtime_yield(void) noexcept {
-    // Ensure we're inside the coroutine world.
-    MINIMK_ASSERT(s0.current != nullptr);
-
-    // Manually switch back to the scheduler stack.
-    minimk_runtime_switch(&s0.current->sp, s0.sp);
+void minimk_runtime_run(void) noexcept {
+    minimk_runtime_scheduler_run(&s0);
 }
 
-void minimk_runtime_nanosleep(uint64_t nanosec) noexcept {
+/// Function to yield the CPU from current to another coroutine.
+static inline void minimk_runtime_scheduler_coroutine_yield(scheduler *sched) noexcept {
     // Ensure we're inside the coroutine world.
-    MINIMK_ASSERT(s0.current != nullptr);
+    MINIMK_ASSERT(sched->current != nullptr);
+
+    // Manually switch back to the scheduler stack.
+    minimk_runtime_switch(&sched->current->sp, sched->sp);
+}
+
+void minimk_runtime_yield(void) noexcept {
+    minimk_runtime_scheduler_coroutine_yield(&s0);
+}
+
+/// Function that suspends the current coroutine until the given timeout expires.
+static inline void //
+minimk_runtime_scheduler_coroutine_suspend_timer(scheduler *sched, uint64_t nanosec) noexcept {
+    // Ensure we're inside the coroutine world.
+    MINIMK_ASSERT(sched->current != nullptr);
 
     // Get the current monotonic clock reading
     uint64_t deadline = minimk_time_monotonic_now();
     deadline = minimk_integer_u64_satadd(deadline, nanosec);
 
-    // Suspend the coroutine
-    minimk_runtime_coroutine_suspend_timer(s0.current, deadline);
-    minimk_runtime_yield();
-    minimk_runtime_coroutine_resume_timer(s0.current);
+    // Suspend
+    minimk_runtime_coroutine_suspend_timer(sched->current, deadline);
+
+    // Schedule
+    minimk_runtime_scheduler_coroutine_yield(sched);
+
+    // Resume
+    minimk_runtime_coroutine_resume_timer(sched->current);
+}
+
+void minimk_runtime_nanosleep(uint64_t nanosec) noexcept {
+    minimk_runtime_scheduler_coroutine_suspend_timer(&s0, nanosec);
 }
 
 /// Internal function to uniformly handle suspending for I/O.
-static inline minimk_error_t suspend_io(minimk_syscall_socket_t sock, short events,
-                                        uint64_t nanosec) noexcept {
+static minimk_error_t minimk_runtime_scheduler_coroutine_suspend_io( //
+        scheduler *sched, minimk_syscall_socket_t sock, short events, uint64_t nanosec) noexcept {
     // Ensure we're inside the coroutine world.
-    MINIMK_ASSERT(s0.current != nullptr);
+    MINIMK_ASSERT(sched->current != nullptr);
 
     // Actually suspend the coroutine
-    minimk_runtime_coroutine_suspend_io(s0.current, sock, events, nanosec);
+    minimk_runtime_coroutine_suspend_io(sched->current, sock, events, nanosec);
 
-    // Suspend and wait for resume
-    minimk_runtime_yield();
+    // Schedule
+    minimk_runtime_scheduler_coroutine_yield(sched);
 
     // Resume the coroutine and mark runnable again
-    return minimk_runtime_coroutine_resume_io(s0.current, sock, events);
+    return minimk_runtime_coroutine_resume_io(sched->current, sock, events);
 }
 
 minimk_error_t minimk_runtime_suspend_read(minimk_syscall_socket_t sock,
                                            uint64_t nanosec) MINIMK_NOEXCEPT {
-    return suspend_io(sock, minimk_syscall_pollin, nanosec);
+    return minimk_runtime_scheduler_coroutine_suspend_io(&s0, sock, minimk_syscall_pollin, nanosec);
 }
 
 minimk_error_t minimk_runtime_suspend_write(minimk_syscall_socket_t sock,
                                             uint64_t nanosec) MINIMK_NOEXCEPT {
-    return suspend_io(sock, minimk_syscall_pollout, nanosec);
+    return minimk_runtime_scheduler_coroutine_suspend_io(&s0, sock, minimk_syscall_pollout,
+                                                         nanosec);
 }
